b0551545f204dd92b6d759092de31850c89e67d2
RedBlaze42 <allairehugo@gmail.com>
2022-07-01 20:55:57 +0200

    Fix sur l'échelle

--- merger.py
+++ merger.py
@@ -20,8 +20,10 @@ def merge_svg(base_data, rocket_path, output_path):
     base_path, rectangle_coords, target_size = base_data["path"], base_data["rectangle_coords"], base_data["rectangle_size"]
     bbox = svg_bbox(rocket_path)
     orig_dims = bbox[1] - bbox[0], bbox[3] - bbox[2]
-    scale_1 = ceil(max(orig_dims[0]/target_size[0], orig_dims[1]/target_size[1]))
-    scale = (mm_per_pix**2)/(scale_1)
+    inverse_scales = orig_dims[1]/(target_size[0]/mm_per_pix), orig_dims[0]/(target_size[1]/mm_per_pix)
+    inverse_scale = max(*inverse_scales)
+    
+    scale = (mm_per_pix**2)/(inverse_scale)
     rocket = svgutils.compose.SVG(rocket_path)
     rocket.rotate(90, 0,0)
     rocket.move(f"{orig_dims[1]+rectangle_coords[0]/scale}mm", f"{orig_dims[0]+rectangle_coords[1]/scale}mm")
